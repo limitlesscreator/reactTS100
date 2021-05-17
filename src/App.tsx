a45513fecd5e00259d38f084b731631ab1af6030
Vladislav V <0428vlad0428@mail.ru>
2021-05-17 13:54:28 +0300

    start with redux :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,12 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./components/Header/Header";
-import {Navbar} from "./components/Navbar/Navbar";
-import {Profile} from "./components/Profile/Profile";
-import {Dialogs} from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
-import {News} from "./components/News/News";
-import {Music} from "./components/Music/Music";
-import {Settings} from "./components/Settings/Settings";
-import {ActionsType, StateType, StoreType} from "./redux/state";
 
 
-export type PropsType = {
-    state: StateType
-    store: StoreType
-    dispatch: (action: ActionsType) => void
-}
-
-const App = (props: PropsType) => {
+const App = () => {
     return (
-        <BrowserRouter>
-            <div className='app-wraper'>
-                <Header/>
-                <Navbar/>
-                <div className='app-wraper-content'>
-                    <Route exact path={'/dialogs'}
-                           render={() => <Dialogs store={props.store}/>}/>
-                    <Route exact path={'/profile'}
-                           render={() => <Profile
-                               profilePage={props.state.profilePage}
-                               dispatch={props.dispatch}
-                           />}/>
-
-                    <Route exact path={'/news'} render={() => <News/>}/>
-                    <Route exact path={'/music'} render={() => <Music/>}/>
-                    <Route exact path={'/settings'} render={() => <Settings/>}/>
-                </div>
-            </div>
-        </BrowserRouter>
+        <div className='App'>
+            start with redux :)
+        </div>
     );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {StateType, store,} from "./redux/state";
 
-// addPost('hey')
-export let rerenderEntireTree = (state:StateType) => {
+
     ReactDOM.render(
         <React.StrictMode>
-            <App state={state} dispatch={store.dispatch.bind(store)} store={store} />
+            <App />
         </React.StrictMode>,
         document.getElementById('root')
     );
-}
 
-rerenderEntireTree(store.getState())
 
-store.subscribe(rerenderEntireTree)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
