c96a72ff0657bb87ba31ed9c6a7c388849070447
Vladislav V <0428vlad0428@mail.ru>
2021-05-19 11:30:32 +0300

    finished 07

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,12 @@
 import React, {} from "react";
-
+import s from '../StylesModule/Main.module.sass'
 export function Main(){
     
     return(
-        <div>
-            <ul>
-                <li>Html</li>
-                <li>Css</li>
-                <li>Js</li>
-                <li>React</li>
-            </ul>
+        <div className={s.main}>
+            <div className={s.title}>Counter + 2.5</div>
+            <div className={s.value}>0</div>
+            <button className={s.add}>+</button>
         </div>
     )
 }
